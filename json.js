aa2c6df241e36362bb363b72d5f559abfa6422f3
dongsongshan <372290807@qq.com>
2016-01-21 17:14:12 +0800

    Update json.js

--- json.js
+++ json.js
@@ -1,32 +1,38 @@
 {
-  0:[{'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题1','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题1','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题1','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题1','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题1','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题1','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题1','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题1','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题1','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'}
+  "0":[{"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题1","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题1","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题1","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题1","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题1","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题1","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题1","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题1","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题1","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"}
   ],
-  1:[{'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题2','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题2','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题2','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题2','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题2','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题2','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题2','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题2','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题2','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'}
+  "1":[{"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题2","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题2","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题2","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题2","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题2","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题2","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题2","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题2","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题2","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"}
   ],
-  2:[{'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题3','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题3','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题3','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题3','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题3','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题3','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题3','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题3','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'},
-  {'gamepic':'http://placeholod.it/70x70/fff','gametit':'这是文章的标题3','time':'2015-12-25','secret':'XBIAOXBIAOXBIAO'}
+  "2":[{"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题3","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题3","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题3","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题3","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题3","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题3","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题3","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题3","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"},
+  {"gamepic":"http://placeholod.it/70x70/fff","gametit":"这是文章的标题3","time":"2015-12-25","secret":"XBIAOXBIAOXBIAO"}
   ]
 }
+
+
+
+
+
+
