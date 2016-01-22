ba8295797638858eb9e38f981830ee8619e20d92
dongsongshan <372290807@qq.com>
2016-01-22 16:12:26 +0800

    Create json2.js

---
+++ json/json2.js
@@ -0,0 +1,32 @@
+{
+  "0":[{"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"}
+  ],
+  "1":[{"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"}
+  ],
+  "2":[{"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"},
+  {"url":"http://www.xbiao.com/","pic":"http://placehold.it/150x150/f00/fff","number":"500"}
+  ]
+}
