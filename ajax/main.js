c1ebcb74bbc674486375c941ab47c2cfa0acb3a8
dongsongshan <dongsongshan1987@gmail.com>
2016-05-25 19:17:05 +0800

    Update main.js

--- ajax/main.js
+++ ajax/main.js
@@ -5,8 +5,8 @@ $(function(){
 	$.ajax({
 		type:"GET",
 		url:"order",
-		success:function(order){
-			$.each(order,function(i,order){
+		success:function(orders){
+			$.each(orders,function(i,order){
 				$list.append('<li>'+ 2 +'</li>');
 			});
 		},
