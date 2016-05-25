9e573880e5cc0abb586f78f1fb253f802db0bf26
dongsongshan <dongsongshan1987@gmail.com>
2016-05-25 19:23:08 +0800

    Update main.js

--- ajax/main.js
+++ ajax/main.js
@@ -5,10 +5,11 @@ $(function(){
 	$.ajax({
 		type:"GET",
 		url:"order",
-		success:function(orders){
-			$.each(orders,function(i,order){
-				$list.append('<li>'+ '2' +'</li>');
-			});
+		success:function(date){
+			console.log(success,date);
+			// $.each(orders,function(i,order){
+			// 	$list.append('<li>'+ '2' +'</li>');
+			// });
 		},
 		error:function(){
 			alert(0);
