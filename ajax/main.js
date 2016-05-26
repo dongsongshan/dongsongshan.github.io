a115e6555474b45cea463165409158168e6113cc
dongsongshan <dongsongshan1987@gmail.com>
2016-05-26 09:19:54 +0800

    Update main.js

--- ajax/main.js
+++ ajax/main.js
@@ -1,7 +1,8 @@
 $(function(){
 
 	var $list = $('#list ul');
-
+	var $name = $('#name').val();
+	var $drink = $('#drink').val();
 	$.ajax({
 		type:"GET",
 		url:"order",
@@ -12,6 +13,18 @@ $(function(){
 			$.each(date,function(i,o){
 				$list.append('<li>name:'+ o.name +';drink:'+ o.drink +'</li>');
 			});
+		},
+		error:function(){
+			alert(0);
+		}
+	});
+	
+	$.ajax({
+		type:'POST',
+		url:"order",
+		date:{"name":$name,"drink":$drink},
+		success:function(){
+			console.log(1);
 		},
 		error:function(){
 			alert(0);
